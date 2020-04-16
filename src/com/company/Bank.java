e715c5041c88f8b75e6961104b1ae683fb812f2a
Vanessa Dixon <vanessaadixon@gmail.com>
2020-04-15 20:52:47 -0400

    added a way for customer to add money

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name, String branch) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+
+    }
+
+    public boolean addCustomer(String branchName, String CustomerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null) {
+            return branch.newCustomer(CustomerName, initialAmount);
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount) {
+        Branch branch = findBranch(branchName);
+        if(branch != null ) {
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+    }
 }
+
